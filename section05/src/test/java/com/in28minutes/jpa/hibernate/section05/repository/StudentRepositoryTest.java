e9b0065e4adcb0133e016643918a73f2aa9ce7be
devYuraKim <dev.yurakim@gmail.com>
2024-06-30 01:17:49 +0900

    S06S25 OneToOne Fetch.EAGER

---
+++ section05/src/test/java/com/in28minutes/jpa/hibernate/section05/repository/StudentRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.in28minutes.jpa.hibernate.section05.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.in28minutes.jpa.hibernate.section05.Section05Application;
+import com.in28minutes.jpa.hibernate.section05.entity.Course;
+import com.in28minutes.jpa.hibernate.section05.entity.Passport;
+import com.in28minutes.jpa.hibernate.section05.entity.Student;
+
+import jakarta.persistence.EntityManager;
+
+@SpringBootTest(classes=Section05Application.class)
+class StudentRepositoryTest {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	StudentRepository repository;
+	
+	@Autowired
+	EntityManager em;
+	
+	@Test
+	public void retrieveStudentAndPassport() {
+		Student student = em.find(Student.class, 20001l);
+		logger.info("student -> {}", student);
+		logger.info("passport -> {}", student.getPassport());
+	}
+	
+	
+	
+}
