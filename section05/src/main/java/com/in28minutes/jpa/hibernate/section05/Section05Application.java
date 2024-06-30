e5555ff5ece59341354f7d56644b64618ae0b97b
devYuraKim <dev.yurakim@gmail.com>
2024-06-30 23:11:22 +0900

    S08S32 ManyToOne adding multiple reviews to a course

--- section05/src/main/java/com/in28minutes/jpa/hibernate/section05/Section05Application.java
+++ section05/src/main/java/com/in28minutes/jpa/hibernate/section05/Section05Application.java
@@ -1,5 +1,8 @@
  package com.in28minutes.jpa.hibernate.section05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.in28minutes.jpa.hibernate.section05.entity.Course;
+import com.in28minutes.jpa.hibernate.section05.entity.Review;
 import com.in28minutes.jpa.hibernate.section05.repository.CourseRepository;
 import com.in28minutes.jpa.hibernate.section05.repository.StudentRepository;
 
@@ -36,7 +40,14 @@ public class Section05Application implements CommandLineRunner {
 		//repository.save(new Course("Microservices in 100 steps"));
 		//repository.playWithEntityManager3();
 		
-		studentRepository.saveStudentWithPassport();
+		//studentRepository.saveStudentWithPassport();
+		
+		//courseRepository.addHardCodedReviewsForCourse();
+		
+		List<Review> reviews = new ArrayList<>();
+		reviews.add(new Review("5", "Great Hands-on stuff"));
+		reviews.add(new Review("5", "Hats-off"));
+		courseRepository.addReviewsForCourse(10003l, reviews);
 	}
 	
 }
--- section05/src/main/java/com/in28minutes/jpa/hibernate/section05/repository/CourseRepository.java
+++ section05/src/main/java/com/in28minutes/jpa/hibernate/section05/repository/CourseRepository.java
@@ -1,11 +1,15 @@
 package com.in28minutes.jpa.hibernate.section05.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.in28minutes.jpa.hibernate.section05.entity.Course;
+import com.in28minutes.jpa.hibernate.section05.entity.Review;
 
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -78,7 +82,35 @@ public class CourseRepository {
 		
 		Course course2 = findById(10001L);
 		course2.setName("JPA in 50 steps - updated");
+	}
+
+	public void addHardCodedReviewsForCourse(){
+		//1.get course 10003
+		Course course = findById(10003l);
+		logger.info("course.getReviews -> {}", course.getReviews());
+		//2.add 2 reviews
+		Review review1 = new Review("5", "first review");
+		course.addReview(review1);
+		review1.setCourse(course);
 		
+		Review review2 = new Review("5", "second review");
+		course.addReview(review2);
+		review2.setCourse(course);
+		
+		//3.save them to DB 
+		em.persist(review1);
+		em.persist(review2);
 	}
 	
+	public void addReviewsForCourse(Long courseId, List<Review> reviews){
+		Course course = findById(courseId);
+		logger.info("course.getReviews -> {}", course.getReviews());
+
+		for(Review review : reviews) {
+			//setting up relationship
+			course.addReview(review);
+			review.setCourse(course);
+			em.persist(review);
+		}
+	}
 }
